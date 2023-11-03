d84eda6c950ca867b60273032b7f5804b0315804
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-11-03 22:34:01 +0100

    Slices: Finished

---
+++ Slices/Slices6/slices6.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func createMatrix(rows, cols int) [][]int {
+
+	matrix := [][]int{}
+
+	for i := 0; i < rows; i++ {
+
+		auxSlice := []int{}
+
+		for j := 0; j < cols; j++ {
+
+			auxSlice = append(auxSlice, i*j)
+
+		}
+
+		matrix = append(matrix, auxSlice)
+
+	}
+
+	return matrix
+
+}
+
+// dont edit below this line
+
+func test(rows, cols int) {
+	fmt.Printf("Creating %v x %v matrix...\n", rows, cols)
+	matrix := createMatrix(rows, cols)
+	for i := 0; i < len(matrix); i++ {
+		fmt.Println(matrix[i])
+	}
+	fmt.Println("===== END REPORT =====")
+}
+
+func main() {
+	test(3, 3)
+	test(5, 5)
+	test(10, 10)
+	test(15, 15)
+}

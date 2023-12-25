ed577867cb440b3be0fee83421fb0ef18abefa88
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-12-25 04:14:45 +0100

    Part 4. Authentication wiith API Keys: Developing

--- PROJECTS/RSSaggregator/handler_user.go
+++ PROJECTS/RSSaggregator/handler_user.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/JoseHurtadoBaeza/GolangCourse-BonusProjects/PROJECTS/RSSaggregator/internal/auth"
 	"github.com/JoseHurtadoBaeza/GolangCourse-BonusProjects/PROJECTS/RSSaggregator/internal/database"
 	"github.com/google/uuid"
 )
@@ -36,5 +37,23 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 404, fmt.Sprintf("Couldn't get user: %v", err))
+		return
+	}
+
 	respondWithJSON(w, 200, databaseUserToUser(user))
+
 }
